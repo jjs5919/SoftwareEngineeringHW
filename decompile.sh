302a24086710a3a6383f96af4b1fad1e935916d0
jjs <jjs5919@nate.com>
2014-04-08 18:50:32 +0900

    import project

---
+++ decompile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+EXT=`echo ${1##*.}`
+echo "$EXT"
+FILENAME=`echo ${1%.*}`
+echo "$FILENAME"
+
+#if [ "$EXT" -ne 'apk' ]
+#then
+#	echo "You have to add apk file"
+#else
+#	echo "Yes"
+#fi
+
+cd /home/android/work
+
+mv $1 $FILENAME.zip
+unzip $FILENAME.zip
+
+mkdir "$FILENAME"Files
+cd "$FILENAME"Files
+cp /home/android/work/$FILENAME/classes.dex .
+dex2jar.sh classes.dex
+
+mkdir "$FILENAME"Classes
+mkdir "$FILENAME"Javas
+cd "$FILENAME"Classes
+
+mv /home/android/work/"$FILENAME"Files/classes_dex2jar.jar .
+jar -xvf classes_dex2jar.jar
+
+cd /home/android/work/"$FILENAME"Files/"$FILENAME"Javas
+find /home/android/work/"$FILENAME"Files -name *.class | while read line
+do
+jad -o -sjava $line
+done
+
+#echo "$1" | grep ".apk" | cut -d . -f 1
+
+#ls | grep ".apk" | cut -d . -f 1 | while read line
+#do
+#echo `ls $line.apk`
+#mv $line.apk $line.zip
+#done
+
+
